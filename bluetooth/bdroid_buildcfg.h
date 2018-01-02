53ebe75ab591da12eefa9d64c793ce4c180ec293
kongwoojin <kongwoojin@naver.com>
2018-01-02 17:18:45 +0900

    ef65: Rename bluetooth device name to IM-A920

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,7 @@
 
 #define BLUETOOTH_QTI_SW TRUE
 
-#define BTM_DEF_LOCAL_NAME   "Vega Secrect UP"
+#define BTM_DEF_LOCAL_NAME   "IM-A920"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
